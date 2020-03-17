00f73976e709b8e8f3503cfb7264832607db5ab0
jorgvergracht <55498433+jorgvergracht@users.noreply.github.com>
2020-03-17 12:35:42 +0100

    Add files via upload

---
+++ security-app/app-security-samengezet/app.js
@@ -0,0 +1,62 @@
+var createError = require("http-errors");
+var express = require("express");
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var AccountRoutes = require('./routes/account_controller');
+var HomeRoutes = require('./routes/home_controller');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+  secret: 'randomstringsessionsecret',
+  resave: true,
+  saveUninitialized: true, 
+  secure:false 
+}));
+
+app.use('/',AccountRoutes.AccountRoutes);
+//app.use("/home", HomeRoutes.HomeRoutes);
+
+app.use(function(req,res,next){
+  if(req.session.username == null || req.session.username.length ==0 ){
+    res.redirect('/register');
+  }
+  else{
+    next();
+  }
+});
+app.use('/home',HomeRoutes.HomeRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+//app.listen(port);
