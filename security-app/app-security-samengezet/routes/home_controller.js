e2c701d3a279b09c2426a9b14dcc4a7e43884cfc
jorgvergracht <55498433+jorgvergracht@users.noreply.github.com>
2020-03-17 03:27:33 +0100

    Add files via upload

---
+++ security-app/app-security-samengezet/routes/home_controller.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
+var path = require('path');
+var HomeRoutes = express.Router();
+
+var correct_path = path.join(__dirname+'/../views/');
+HomeRoutes.get('/',function(req,res){
+    let username = req.session.username;
+    res.render('index',{user_name:username});
+});
+HomeRoutes.post('/logout',function(req,res){
+    req.session.username=null;
+    res.redirect('/login');
+});
+
+
+module.exports = {"HomeRoutes" : HomeRoutes};
